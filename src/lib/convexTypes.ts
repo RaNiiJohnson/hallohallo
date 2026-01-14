b767a53693934675dcac8d7be8d7d9a63db2e0de
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2026-01-14 20:24:43 +0300

    adding convextype

---
+++ src/lib/convexTypes.ts
@@ -0,0 +1,9 @@
+import { api } from "@convex/_generated/api";
+
+export type AuthUser = NonNullable<typeof api.auth.getCurrentUser._returnType>;
+
+type JobsReturn = typeof api.jobs.getJobs._returnType;
+
+export type JobOfferListItem = JobsReturn[number];
+
+export type JobOfferDetails = typeof api.jobs.getJobWithContact._returnType;
