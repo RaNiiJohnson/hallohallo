ff61343ad1eb6b5097079063943115afcfd7120f
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2026-01-15 22:26:40 +0300

    feat: add `createJob` mutation for authenticated users and remove `instrumentation.ts`.

--- convex/jobs.ts
+++ convex/jobs.ts
@@ -1,5 +1,6 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+import { authComponent } from "./auth";
 
 export const getJobWithContact = query({
   args: { id: v.id("JobOffer") },
@@ -25,6 +26,44 @@ export const getJobs = query({
   },
 });
 
+export const createJob = mutation({
+  args: {
+    title: v.string(),
+    type: v.string(),
+    location: v.optional(
+      v.object({
+        lat: v.number(),
+        lng: v.number(),
+      })
+    ),
+    contractType: v.string(),
+    city: v.string(),
+    duration: v.string(),
+    startDate: v.string(),
+    company: v.string(),
+    description: v.string(),
+    certificates: v.array(v.string()),
+    salary: v.string(),
+    authorId: v.string(),
+    createdAt: v.number(),
+    updatedAt: v.number(),
+  },
+  handler: async (ctx, args) => {
+    const user = await authComponent.safeGetAuthUser(ctx);
+
+    if (!user) {
+      throw new Error("Not authenticated");
+    }
+
+    const job = await ctx.db.insert("JobOffer", {
+      ...args,
+      authorId: user._id,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    });
+    return job;
+  },
+});
 // const existing = await ctx.db
 //   .query("JobBookmark")
 //   .withIndex("by_user_job", (q) =>
