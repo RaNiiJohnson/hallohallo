6ea1aa662ed95381daf80c48a285abed976d4ab5
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2026-05-23 11:40:55 +0300

    refactor: remove unused locale hook from LocaleSwitcher component

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -1,8 +1,5 @@
-import { useLocale } from "next-intl";
 import LocaleSwitcherSelect from "./locale-switcher-select";
 
 export default function LocaleSwitcher() {
-  const locale = useLocale();
-
-  return <LocaleSwitcherSelect defaultValue={locale} />;
+  return <LocaleSwitcherSelect />;
 }
