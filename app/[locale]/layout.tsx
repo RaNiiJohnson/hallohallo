34240835e036770f6e591508e523fe43c4fdbde0
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2026-09-13 21:13:59 +0300

    change default theme to dark

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -57,7 +57,7 @@ export default async function RootLayout({
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
